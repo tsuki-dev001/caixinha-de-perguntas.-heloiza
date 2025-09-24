8ac9ef9108ddc4be9b5843fe6f760be256e92bae
tsuki-dev001 <c.heloiza@escola.pr.gov.br>
2025-09-24 11:07:17 -0300

    .

---
+++ Perguntas.js
@@ -0,0 +1,11 @@
+CriaCartão(
+    categoria,
+    pergunta,
+    resposta,
+)
+CriaCartão(
+    'Dead by Daylight',
+    'vc sabe oq é dbd(Dead by Daylight)?',
+  'O Dead by Daylight é um jogo de terror de sobrevivência, onde um jogador assume o papel do Assassino e os outros quatro jogadores atuam como Sobreviventes. O objetivo dos  Sobreviventes é  escapar, enquanto o Assassino tem que caçá-los.'
+)
+CriaCartão(
---
+++ app.js
@@ -0,0 +1,7 @@
+ function CriaCartão(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartão = document.createElement('article')
+    cartão.className = 'cartão'
+    cartão.innerHTML =
+    
+}
