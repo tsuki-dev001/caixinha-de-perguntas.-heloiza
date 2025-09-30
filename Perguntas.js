65590fd43493a2f4d872eabc7a1ea4e6d57702d7
tsuki-dev001 <c.heloiza@escola.pr.gov.br>
2025-09-30 09:09:28 -0300

    .

--- Perguntas.js
+++ Perguntas.js
@@ -9,3 +9,10 @@ CriaCartão(
   'O Dead by Daylight é um jogo de terror de sobrevivência, onde um jogador assume o papel do Assassino e os outros quatro jogadores atuam como Sobreviventes. O objetivo dos  Sobreviventes é  escapar, enquanto o Assassino tem que caçá-los.'
 )
 CriaCartão(
+  'Pergunta 2',
+  'O que é o objetivo dos Sobreviventes?',
+  'Os Sobreviventes devem consertar cinco geradores para abrir os portões de saída e escapar. Eles também podem escapar através do Alçapão, caso sejam os últimos a permanecer na partida'
+)
+CriaCartão(
+  'pergunta 3'
+  ''
--- app.js
+++ app.js
@@ -1,7 +1,25 @@
- function CriaCartão(categoria, pergunta, resposta) {
+function CriaCartão(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartão = document.createElement('article')
+    let respostaEstaVisivel = false
+    function viraCartão() {
+        respostaEstaVisivel = !respostaEstaVisivel
+    }
+    container.appendChild(cartão)
+    let respostaEstaVisivel = true
     cartão.className = 'cartão'
     cartão.innerHTML =
-    
+        <div class="cartão_conteudo">
+            <h3>Conhecimento gerais</h3>
+            <div class="cartão_conteudo_pergunta">
+                <p>qual a montanha mais alta do mundo</p>
+            </div>
+            <div class="cartão_conteudo_resposta">
+                <p>monte everest</p>
+            </div>
+        </div>
+        ,
+
+
+        container.appendChild(cartão)
 }
