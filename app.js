e161805b4f294d5e7862bcd208447d3eabcd2b64
tsuki-dev001 <c.heloiza@escola.pr.gov.br>
2025-10-22 10:50:28 -0300

    .

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 function CriaCartão(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartão = document.createElement('article')
-   cartão.className= 'cartão'
-    cartão.InnerHTML =
+    cartão.className = 'cartão'
+
+    cartão.InnerHTML = `
     <div class="cartão_conteudo">
     <h3>${categoria}</h3>
     <div class="cartão_conteudo_pergunta">
         <p>${pergunta}</p>
 </div>
-<div class=ca</div>
+<div class='cartão_conteudo_respota">
+<p>${resposta}
+</div>
+<div/>
+`
+container.appendChild(cartão)
+
+let respotaEstaVisiel= false
+function viraCartã(){
+    respotaEstaVisiel = !respotaEstaVisiel
+    cartão.classList.toggle('ative', respotaEstaVisiel)
+}
+cartão.addEventListener('click', viraCartão)
+}
+e
