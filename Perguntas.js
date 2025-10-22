9643f00bc1cedb3c0e115ecdac7d4f3fb5a2a43d
tsuki-dev001 <c.heloiza@escola.pr.gov.br>
2025-10-22 08:19:44 -0300

    open in web .

--- Perguntas.js
+++ Perguntas.js
@@ -13,6 +13,23 @@ CriaCartão(
   'O que é o objetivo dos Sobreviventes?',
   'Os Sobreviventes devem consertar cinco geradores para abrir os portões de saída e escapar. Eles também podem escapar através do Alçapão, caso sejam os últimos a permanecer na partida'
 )
+ CriaCartão(
+  'pergunta 3',
+  'E qual é o objetivo do Assassino?',
+' No jogo Dead by Daylight, o objetivo do Assassino é caçar e sacrificar os quatro Sobreviventes. Ele precisa encontrá-los, feri-los e, por fim, colocá-los em um dos ganchos espalhados pelo mapa para serem sacrificados à Entidade. A vitória do Assassino só é garantida quando todos os Sobreviventes são eliminados.'
+)
 CriaCartão(
-  'pergunta 3'
-  ''
+  'pergunta 4',
+  ' O que acontece quando um Sobrevivente é pendurado em um gancho?',
+' No jogo Dead by Daylight, o objetivo do Assassino é caçar e sacrificar os quatro Sobreviventes. Ele precisa encontrá-los, feri-los e, por fim, colocá-los em um dos ganchos espalhados pelo mapa para serem sacrificados à Entidade. A vitória do Assassino só é garantida quando todos os Sobreviventes são eliminados.'
+)
+CriaCartão(
+  'pergunta 5',
+  ' oq são perks?',
+  'Perks (Vantagens) são habilidades especiais que sobreviventes e assassinos equipam no Dead by Daylight (DBD) para conceder bônus e efeitos que influenciam a jogabilidade durante a partida..'
+)
+CriaCartão(
+  'pergunta 6',
+  'Os Assassinos (Killers) também têm suas próprias Perks (Vantagens)?',
+  'sim, Cada Assassino, ao ser desbloqueado, possui três Perks "Ensináveis" (Teachable Perks) exclusivas que podem ser aprendidas por todos os outros Assassinos em sua conta através da Teia de Sangue, assim como acontece com os Sobreviventes..'
+)
--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 function CriaCartão(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartão = document.createElement('article')
-    let respostaEstaVisivel = false
-    function viraCartão() {
-        respostaEstaVisivel = !respostaEstaVisivel
-    }
-    container.appendChild(cartão)
-    let respostaEstaVisivel = true
-    cartão.className = 'cartão'
-    cartão.innerHTML =
-        <div class="cartão_conteudo">
-            <h3>Conhecimento gerais</h3>
-            <div class="cartão_conteudo_pergunta">
-                <p>qual a montanha mais alta do mundo</p>
-            </div>
-            <div class="cartão_conteudo_resposta">
-                <p>monte everest</p>
-            </div>
-        </div>
-        ,
-
-
-        container.appendChild(cartão)
-}
+   cartão.className= 'cartão'
+    cartão.InnerHTML =
+    <div class="cartão_conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartão_conteudo_pergunta">
+        <p>${pergunta}</p>
+</div>
+<div class=ca</div>
